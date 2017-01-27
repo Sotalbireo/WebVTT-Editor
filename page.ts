a38d451f4ec2266e6d808299f7de7d62d542d54b
Sota NAKAGAMI <athene609.cyl@gmail.com>
2017-01-27 20:33:53 +0900

    いろいろやりました（クソｃｏｍｍｉｔ）

--- page.ts
+++ page.ts
@@ -5,12 +5,12 @@ const prefix = 'WEBVTT'
 const filePath = path.join(__dirname, 'subtitle.ja.vtt')
 let hogefuga = 1
 
-const formatTime = (t:number, f=true)=>{
-	let h = ('0' + Math.floor(t/3600)%60).slice(-2)
-	let m = ('0' + Math.floor(t/60)).slice(-2)
-	let s = ('0' + Math.floor(t*1000%60000)/1000).slice(-6)
-	s = (f)? s: s.substr(0, s.length-2)
-	return (f)? `${h}:${m}:${s}`: ``
+const formatTime = (t:number, f=false)=>{
+	let h  = ('0' + Math.floor(t/3600)%60).slice(-2)
+	let m  = ('0' + Math.floor(t/60)%60).slice(-2)
+	let hm = ('00' + Math.floor(t/60)).slice(-3)
+	let s  = ('0' + Math.floor(t*1000%60000)/1000).slice(-6)
+	return (f)? `${hm}:${s}`: `${h}:${m}:${s.slice(0,2)}`
 }
 let getCurrentTime = (indent=0)=>{
 	return document.querySelectorAll('video')[0].currentTime + indent
@@ -34,21 +34,21 @@ let startPause = ()=>{
 		v.pause()
 	}
 }
-let preb10s = ()=>{
+let prev10s = ()=>{
 	let v = document.querySelectorAll('video')[0]
 	v.currentTime -= 10
 }
 
 let appendTime = ()=>{
 	let dom = document.querySelectorAll('textarea')[0]
-	dom.textContent += `${formatTime(getCurrentTime(-0.5))}
-${hogefuga++}
+	dom.textContent += `${formatTime(getCurrentTime(-0.5), true)}
+Loem ipsum: ${hogefuga++}
 
-${formatTime(getCurrentTime())}${arrow}`
+${formatTime(getCurrentTime(), true)}${arrow}`
 }
 
 let overwriteFile = ()=>{
-	if(confirm('Overwrite right?')){
+	if(confirm('Overwrite, right?')){
 		let data = document.querySelectorAll('textarea')[0].value
 		fs.writeFile(filePath, data, (err:any)=>{
 			if(err) throw err
@@ -57,7 +57,7 @@ let overwriteFile = ()=>{
 }
 
 const textareaResize = ()=>{
-	let d = document.querySelectorAll('textarea')[0]
+	let d = document.querySelectorAll('#Textarea')[0]
 	const h = window.innerHeight
 	const rect = d.getBoundingClientRect()
 	d.setAttribute('height', (h - 15 - rect.top) + 'px')
@@ -72,12 +72,15 @@ let preInit = ()=>{
 	if(!isExistFile(filePath)){
 		const data = `${prefix}
 
-00:00.000${arrow}00:00:05.999
+000:00.000${arrow}000:05.999
 This is "subtitle.ja.vtt"
 `
 		fs.writeFile(filePath, data, (err:any,msg:any)=>{
-			if(err) throw err
-			console.log(msg)
+			if(err){ throw err
+			}else{
+				location.reload(true)
+			}
+
 		})
 	}
 }
@@ -88,14 +91,14 @@ let Init = ()=>{
 	textareaResize()
 
 	document.querySelectorAll('#StartPause')[0].addEventListener('click',startPause,false)
-	document.querySelectorAll('#Preb10')[0].addEventListener('click',preb10s,false)
+	document.querySelectorAll('#Prev10')[0].addEventListener('click',prev10s,false)
 	document.querySelectorAll('#Overwrite')[0].addEventListener('click',overwriteFile,false)
 	document.querySelectorAll('#Append')[0].addEventListener('click',appendTime,false)
 
 	document.querySelectorAll('textarea')[0].innerText = fs.readFileSync(filePath,'utf8')
 
 	document.querySelectorAll('video')[0].addEventListener('timeupdate', setCurrentTime, false)
-	// window.addEventListener('resize', textareaResize, false)
+	window.addEventListener('resize', textareaResize, false)
 }
 
 
